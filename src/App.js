26c2e6473a36be31b0ee4261771c55f789b4638f
Luis Arturo García Gonzalez <lgarcia103@alumnos.uaq.mx>
2020-11-13 12:45:35 -0600

    Moved Login and Register to /screens and deleted react template imgs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import "./App.css";
 import Header from "./components/Header";
-import React, { Component } from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import Login from './components/Login.js';
-import Register from './components/Register.js';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from './screens/Login.js';
+import Register from './screens/Register.js';
 
 function App() {
   return (
--- src/screens/Register.js
+++ src/screens/Register.js
@@ -2,8 +2,7 @@ import React, { useState } from "react";
 import "./Register.css";
 import shirts from "../img/MainShirts.png";
 import line from "../img/line.png";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import Login from "./Login.js";
+import { Link } from "react-router-dom";
 
 function Register(){
   const [username, setUsername] = useState(undefined);
@@ -21,52 +20,52 @@ function Register(){
   }
 
     return (
-      <div class="flex mb-4 background">
+      <div className="flex mb-4 background">
         {/* Titles */}
-        <div class="fit flex w-1/2 flex-col justify-center">
-          <div class="px-32 py-5 text-center">
-            <h1 class="welcome text-5xl">Join Ignite!</h1>
-            <h3 class="px-12 text-xl">
+        <div className="fit flex w-1/2 flex-col justify-center">
+          <div className="px-32 py-5 text-center">
+            <h1 className="welcome text-5xl">Join Ignite!</h1>
+            <h3 className="px-12 text-xl">
               Create an account to generate custom designs
             </h3>
           </div>
           {/* Inputs */}
-          <div class="login px-32 py-2 flex flex-col">
+          <div className="login px-32 py-2 flex flex-col">
             <input 
-              class="px-5 py-2" 
+              className="px-5 py-2" 
               placeholder="Username" 
               onChange={event => setUsername(event.target.value)}></input>
             <input 
-              class="px-5 py-2 mt-5" 
+              className="px-5 py-2 mt-5" 
               placeholder="Password" 
               onChange={event => setPassword(event.target.value)}></input>
             <input
-              class="px-5 py-2 mt-5"
+              className="px-5 py-2 mt-5"
               placeholder="Confirm Password"
               onChange={event => setPasswordConfirm(event.target.value)}
             ></input>
           </div>
           {/* Create Button */}
-          <div class="px-32 py-5 flex flex-col botones" onClick={signUp}>
-            <button class="px-5 py-3 boton_verde">Create Account</button>
+          <div className="px-32 py-5 flex flex-col botones" onClick={signUp}>
+            <button className="px-5 py-3 boton_verde">Create Account</button>
           </div>
           {/* Privacy Policy */}
-          <div class="privacy px-32 py-2 flex flex-row">
+          <div className="privacy px-32 py-2 flex flex-row">
             <input 
-              class="checkbox mt-1" 
+              className="checkbox mt-1" 
               type="checkbox" 
               onChange={event => setAcceptTerms(event.target.value)}/>
-            <p class="ml-5 text-center">
+            <p className="ml-5 text-center">
               I accept Ignite's Terms of Service and Privacy Policy
             </p>
           </div>
-          <img class="h-auto px-32 py-4" src={line} alt="Line" />
+          <img className="h-auto px-32 py-4" src={line} alt="Line" />
           {/* Sign up */}
-          <div class="forgot px-32 py-3 text-right flex flex-col">
-            <button class="px-5 py-3 boton_naranja">
+          <div className="forgot px-32 py-3 text-right flex flex-col">
+            <button className="px-5 py-3 boton_naranja">
               Sign up with DuckDuckGo
             </button>
-            <h4 class="mt-5 text-center">
+            <h4 className="mt-5 text-center">
               Already have an account? Login{" "}
               <Link to={"/login"}>
                 <b>
@@ -78,8 +77,8 @@ function Register(){
         </div>
 
         {/* Shirt Image */}
-        <div class="fit flex w-1/2 items-center background">
-          <div class="px-10 py-10">
+        <div className="fit flex w-1/2 items-center background">
+          <div className="px-10 py-10">
             <img src={shirts} alt="Shirts" />
           </div>
         </div>
