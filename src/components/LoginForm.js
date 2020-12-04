5031410814d9fd8a53d6f639c64528acbcab20a0
Raúl <raulguzmanguerrero@outlook.com>
2020-12-04 02:00:18 -0600

    Logout when login form mounts

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,11 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { LocalForm, Control, Errors } from "react-redux-form";
-import { useAuth } from "reactfire";
+import { useHistory } from "react-router-dom";
+import { useAuth, useFirestore, useFirebaseApp } from "reactfire";
 import 'firebase/auth';
 import 'firebase/database';
+import firebasic from "firebase";
 
 function LoginForm(){
     const auth = useAuth();
+    const firestore = useFirestore();
+    const history = useHistory();
+    const firebase = useFirebaseApp();
+
+    useEffect(() => {
+        firebase.auth().signOut();
+    }, []);
 
     const [email, setEmail] = useState(undefined);
     const [password, setPassword] = useState(undefined);
@@ -17,6 +26,33 @@ function LoginForm(){
         auth.signInWithEmailAndPassword(email, password)
         .then((result) => {console.log(result);})
         .catch((error) => {alert(error)});
+
+        history.push('account');
+    };
+
+    const loginWithGoogle = () => {
+        firebase.auth().signOut();
+
+        let provider = new firebasic.auth.GoogleAuthProvider();
+        provider.addScope('https://www.googleapis.com/auth/userinfo.profile');
+
+        firebase.auth().signInWithPopup(provider).then(result => {
+            // Get user info returned by Google's API. result.user is
+            let googleData = result.user.providerData[0];
+            let id = googleData.uid;
+
+            firestore.collection('users').doc(id).set({
+                gens_remaining: 5,
+                name: googleData.displayName
+            });
+            result.user.updateProfile({displayName: googleData.displayName});
+
+            history.push('account');
+
+        }).catch(error => {
+            alert(error);
+        });
+
     };
 
     return(
@@ -48,7 +84,7 @@ function LoginForm(){
             </div>
             <div className="px-32 py-5 flex flex-col botones">
               <button className="px-5 py-2 boton_verde" type="submit">Login</button>
-              <button className="px-5 py-2 boton_naranja mt-5 ">
+              <button className="px-5 py-2 boton_naranja mt-5" type="button" onClick={loginWithGoogle}>
                 Login with Google
               </button>
             </div>
